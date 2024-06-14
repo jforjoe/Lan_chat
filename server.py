631848340105b8a5e693259b644c473908f4fe5b
jforjoe <joelsiby2003@gmail.com>
2024-06-14 15:11:06 +0530

    adding features and dealing with bugs , bugs still prevailing

--- server.py
+++ server.py
@@ -2,7 +2,8 @@ import socket
 import threading
 
 host = socket.gethostbyname(socket.gethostname())
-port = 0
+#port = 0                               
+port = 55555                              # hve kept this for testing purposes, gotta keep it at 0 for implementation
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
@@ -13,15 +14,25 @@ server.listen()
 clients = []
 aliases = []
 
+'''
+def handle_msg(message,sender=None):
+    #handle the messages that are empty characters
 
-def broadcast(message):
+    while message != "":
+        sender.send('Please enter some characters!!\n'.encode('utf-8'))
+        message=sender.recv(1024).decode('utf-8')
+    
+    return message
+'''
+
+#handles sending messages to the clients
+def broadcast(message, sender=None):
+    
+    '''we need to handle the handle the exception of 
+    the client leaving the chatroom and hence the error'''
     for client in clients:
-        client.send(message)
-
-def send_msg(client,message):
-    for user in clients:
-        if user != client:
-            user.send(message)
+        if client != sender:
+            client.send(message)
 
 
 
@@ -29,7 +40,7 @@ def handle_client(client):
     while True:
         try:
             message = client.recv(1024)
-            send_msg(client,message)
+            broadcast(message,sender=client)
         except:
             index = clients.index(client)
             clients.remove(client)
@@ -40,6 +51,7 @@ def handle_client(client):
             aliases.remove(alias)
             break
 
+
 def receive():
     while True:
         print("Server is Listening ......")
@@ -47,8 +59,8 @@ def receive():
         print(f"Connection established {address[0]}:{address[1]}")
         
         client.send('alias?'.encode('utf-8'))
-        alias = client.recv(1024)
-        alias=alias.decode('utf-8')
+        #alias = handle_msg(client.recv(1024).decode('utf-8'),client)
+        alias = client.recv(1024).decode('utf-8')
         aliases.append(alias)
 
         broadcast(f'{alias} has entered the chatroom ......'.encode('utf-8'))
@@ -56,7 +68,6 @@ def receive():
         clients.append(client)
         print(f'alias : {alias}')
         print('------------------------------------------------------------')
-        
         
         client.send("You are now connected!".encode('utf-8'))
         thread = threading.Thread(target=handle_client, args=(client,))
