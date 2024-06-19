1df01705c826fcb9534af7e5eb82243610eb0fdb
Tinu205 <dazleroger404@gmail.com>
2024-06-19 22:51:50 +0530

    Completed class in server side and add a line to sent upd message to the client

--- server.py
+++ server.py
@@ -1,101 +1,88 @@
 import socket
 import threading
 
-host = socket.gethostbyname(socket.gethostname())
-#port = 0                               
-port = 55555                              # hve kept this for testing purposes, gotta keep it at 0 for implementation
+class ChatServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients = []
+        self.aliases = []
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server_address = server.getsockname()
-print(f'Server is Running on {server_address[0]}:{server_address[1]}')
-server.listen()
+    def start(self):
+        self.server.bind((self.host, self.port))
+        self.server.listen()
+        print(f'Server is running on {self.host}:{self.port}')
 
-clients = []
-aliases = []
+        while True:
+            print("Server is listening ......")
+            client, address = self.server.accept()
+            print(f"Connection established from {address[0]}:{address[1]}")
+            
+            client.send('alias?'.encode('utf-8'))
+            alias = client.recv(1024).decode('utf-8')
+            self.aliases.append(alias)
 
+            self.broadcast(f'{alias} has entered the chatroom ......'.encode('utf-8'))
 
+            self.clients.append(client)
+            print(f'Alias: {alias}')
+            print('------------------------------------------------------------')
+            
+            client.send("You are now connected!".encode('utf-8'))
+            thread = threading.Thread(target=self.handle_client, args=(client,))
+            thread.start()
 
-
-def action(message, client):
-    
-    '''here the function excecutes the requirements for the client request
-    eg: exit'''
-
-    msg = message.decode('utf-8').split(' ')
-
-    if msg[-1] == 'quit_':
-        client.send('You have been disconnected !!!'.encode('utf-8'))
-
-
-
-        #Need to make this portion modular it is repeated ...( handle_client() )... gotta work on this later
-
-        index = clients.index(client)
-        clients.remove(client)
-        client.close()
-        alias = aliases[index]
-        broadcast(f'{alias} has left the chat !'.encode('utf-8'))
-        aliases.remove(alias)
-    
-    else:
-        pass
-
-
-#handles sending messages to the clients
-def broadcast(message, sender=None):
-    
-    '''we need to handle the handle the exception of 
-    the client leaving the chatroom and hence the error'''
-
-    for client in clients:
-        if client != sender:
-
+    def handle_client(self, client):
+        while True:
             try:
-                client.send(message)
-            except:
-                print(f'{client.getpeername()} disconnected during message brodcast !!!')
+                message = client.recv(1024)
+                if not message:
+                    break  # If message is empty, client has disconnected
 
+                self.action(message, client)
+                self.broadcast(message, sender=client)
+            except Exception as e:
+                print(f"Exception occurred in handle_client: {e}")
+                break
 
+        self.disconnect_client(client)
 
-def handle_client(client):
-    while True:
+    def action(self, message, client):
+        msg = message.decode('utf-8').split(' ')
+        if msg[-1] == 'quit_':
+            client.send('You have been disconnected !!!'.encode('utf-8'))
+
+    def disconnect_client(self, client):
         try:
-            message = client.recv(1024)
-
-            action(message,client)  # Suppose the client wants some things to be excecuted and the requirement can be from server side
-
-            broadcast(message,sender=client)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-
+            index = self.clients.index(client)
+            alias = self.aliases[index]
+            self.broadcast(f'{alias} has left the chat !'.encode('utf-8'))
+            self.clients.remove(client)
+            self.aliases.remove(alias)
             client.close()
-            alias = aliases[index]
-            broadcast(f'{alias} has left the chat !'.encode('utf-8'))
-            aliases.remove(alias)
-            break
 
+            # Notify client via UDP
+            udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            client_address = client.getpeername()  # Get client's address
+            disconnect_message = 'You have been disconnected from the chat server.'.encode('utf-8')
+            udp_socket.sendto(disconnect_message, client_address)
+            udp_socket.close()
 
-def receive():
-    while True:
-        print("Server is Listening ......")
-        client, address = server.accept()
-        print(f"Connection established {address[0]}:{address[1]}")
-        
-        client.send('alias?'.encode('utf-8'))
-        alias = client.recv(1024).decode('utf-8')
-        aliases.append(alias)
+        except ValueError:
+            pass  # Handle case where client not found in list
 
-        broadcast(f'{alias} has entered the chatroom ......'.encode('utf-8'))
-
-        clients.append(client)
-        print(f'alias : {alias}')
-        print('------------------------------------------------------------')
-        
-        client.send("You are now connected!".encode('utf-8'))
-        thread = threading.Thread(target=handle_client, args=(client,))
-        thread.start()
+    def broadcast(self, message, sender=None):
+        for cl in self.clients:
+            if cl != sender:
+                try:
+                    cl.send(message)
+                except Exception as e:
+                    print(f"Error broadcasting message to {cl.getpeername()}: {e}")
 
 if __name__ == "__main__":
-    receive()
+    host = socket.gethostbyname(socket.gethostname())
+    port = 55555  # Adjust this port as needed
+
+    server = ChatServer(host, port)
+    server.start()
